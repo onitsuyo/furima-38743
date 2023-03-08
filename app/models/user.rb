4d5f103f8187794e5c79a67bd2ecd6b0e677d2f5
onitsuyo <oo.smilyyyyy@gmail.com>
2023-03-08 21:35:24 +0900

    モデル単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,19 @@ class User < ApplicationRecord
   has_many :documents
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶ一]+\z/ } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
   validates :birthday, presence: true
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
 end
