e83fd7e06ace35f81fc0a700948c5ea4cbc881cb
onitsuyo <oo.smilyyyyy@gmail.com>
2023-03-11 18:35:26 +0900

    ActiveStrageの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,11 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :explanation, :category_id, :condition_id, :cost_responsibility_id, :prefecture_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
 end
