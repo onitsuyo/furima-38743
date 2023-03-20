dc0e2c85f0da65ca3a126beab63ddd547bea54d3
onitsuyo <oo.smilyyyyy@gmail.com>
2023-03-20 17:22:09 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,19 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :cost_responsibility
+  belongs_to :prefecture
+  belongs_to :shipping_day
+
   has_one_attached :image
 
   validates :name, presence: true
   validates :explanation, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :cost_responsibility_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :shipping_day_id, presence: true
-  validates :price, presende: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :cost_responsibility_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price, presence: true
 end
